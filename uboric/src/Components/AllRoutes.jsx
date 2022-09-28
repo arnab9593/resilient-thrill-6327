e94ba85a27db4a36a3c3649aafa73a820a343a4b
arnab9593 <arnabpal679@gmail.com>
2022-09-28 18:33:45 +0530

    day 2 initial work

--- uboric/src/Components/AllRoutes.jsx
+++ uboric/src/Components/AllRoutes.jsx
@@ -5,6 +5,7 @@ import AboutUs from "../Pages/AboutUs";
 import Shop from "../Pages/Shop";
 import Categories from "../Pages/Categories";
 import ContactUs from "../Pages/ContactUs";
+import SignIn from "../Pages/SignIn";
 
 const AllRoutes = () => {
 
@@ -15,6 +16,7 @@ const AllRoutes = () => {
             <Route path="/shop" element={<Shop></Shop>}></Route>
             <Route path="/categories" element={<Categories></Categories>}></Route>
             <Route path="/contactus" element={<ContactUs></ContactUs>}></Route>
+            <Route path="/signin" element={<SignIn></SignIn>}></Route>
         </Routes>
     )
 
--- uboric/src/Pages/Navbar.jsx
+++ uboric/src/Pages/Navbar.jsx
@@ -1,4 +1,21 @@
 import { Link } from "react-router-dom"
+import "../Pages/CSS/Navbar.css"
+import React from "react"
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Button,
+    Input,
+    Image
+} from '@chakra-ui/react'
+
+
 
 const links = [
     { path: "/", title: 'Home ' },
@@ -10,6 +27,8 @@ const links = [
 ]
 
 const Navbar = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
     return (
         <>
             <div className="firstchild_nav">
@@ -26,10 +45,35 @@ const Navbar = () => {
                 </div>
                 <div className="secondchild_nav_profiles">
                     <img src="https://cdn-icons-png.flaticon.com/512/2811/2811806.png" alt="search" />
-                    <img src="https://cdn-icons-png.flaticon.com/512/2207/2207595.png" alt="profile" />
+                    <Image src="https://cdn-icons-png.flaticon.com/512/2207/2207595.png" onClick={onOpen}></Image>
+                    <Drawer
+                        isOpen={isOpen}
+                        placement='right'
+                        onClose={onClose}
+                        finalFocusRef={btnRef}
+                    >
+                        <DrawerOverlay />
+                        <DrawerContent>
+                            <DrawerCloseButton />
+                            <DrawerHeader borderBottomWidth='1px'>Sign in</DrawerHeader>
+
+                            <DrawerBody>
+                                <Input placeholder='Username' />
+                                <Input placeholder='Password' />
+                            </DrawerBody>
+
+                            <DrawerFooter>
+                                <Button variant='outline' mr={3} onClick={onClose}>
+                                    Cancel
+                                </Button>
+                                <Button colorScheme='blue'>Save</Button>
+                            </DrawerFooter>
+                        </DrawerContent>
+                    </Drawer>
                     <img src="https://cdn-icons-png.flaticon.com/512/535/535285.png" alt="wishlist" />
                     <img src="https://cdn-icons-png.flaticon.com/512/3034/3034002.png" alt="cart" />
                 </div>
+
             </div>
         </>
     )
