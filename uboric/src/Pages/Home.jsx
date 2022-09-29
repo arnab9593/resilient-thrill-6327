4659f1552f8c82ca8f0c0c2115e545a66480a740
arnab9593 <arnabpal679@gmail.com>
2022-09-29 14:41:39 +0530

    day 3 work release 3

--- uboric/src/Pages/Home.jsx
+++ uboric/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Slider from "../Components/Slider/Slider"
 import "../Pages/CSS/Homepage.css"
 import { NewArrivals, SwiperFunc } from "../Components/Swiper/Swiper"
 import { BestSellingProduct } from "../Components/Swiper/Swiper"
+import { Button } from "@chakra-ui/react"
 const Home = () => {
     return (
         <>
@@ -53,6 +54,21 @@ const Home = () => {
                 <div className="hp_description">
                     <h1>The Ultimate Domestic Online Shopping Experience Platform in India</h1>
                     <p>Online shopping with Uboric is quick, convenient and trouble-free. You can shop for the desired product right from the comfort of your home and get them delivered straight to your doorstep. Uboric offers you the chance to choose top branded products sitting in the comfort of your homes and just clicking on your requirements to get it delivered at your doorstep. Your search for the latest trending variety of unique products ends right here. A wide range of international products from global brands are available at your fingertips. We provide you with a world-class online shopping experience, along with superior service, to suit all your specific requirements. Our products are very reasonably priced and are not easily available elsewhere. Our high-end technology-based systems, combined with a human approach, ensure that you have an amazing and blissful online shopping experience with us. Our emphasis on customer delight drives every activity we undertake to provide you an ultimate, hassle-free and pleasant shopping experience. Our diverse categories of products comprise of fashion & jewellery, mobiles and tablets, home and furniture, electronics, health care and supplements, sports and fitness, beauty and perfumes. We offer almost every variety of product that your heart desires. Just Go Ahead and Explore the magical world of online shopping with Uboric! 24×7 Customer Care: For all your queries and concerns regarding your shopping orders. </p>
+                </div>
+
+                <div className="benefitsOfShopping">
+                    <div className="benefitsOfShopping1">
+                        <h1>Benefits Of Shopping</h1>
+                        <p>User-Friendly</p>
+                        <p>Safe & Secure Shopping</p>
+                        <p>Check Out Closely</p>
+                        <p>Multiple Payment Options</p>
+                        <p>Order History</p>
+                        <Button color="white" backgroundColor="black">Shop Now</Button>
+                    </div>
+                    <div className="benefitsOfShopping2">
+                        <img src="https://www.uboric.com/wp-content/uploads/2022/08/61OR0aPLJAL._SL1500_-670x575.jpg" alt="" />
+                    </div>
                 </div>
             </div>
         </>
