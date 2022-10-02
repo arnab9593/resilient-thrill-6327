ea0b2d21f599b4f2e3880fed4ba81d50b8d41ad4
arnab9593 <arnabpal679@gmail.com>
2022-10-02 10:24:19 +0530

    day 5 release

--- uboric/src/Pages/AboutUs.jsx
+++ uboric/src/Pages/AboutUs.jsx
@@ -1,7 +1,30 @@
-
+import { Box, Heading, Image, Text } from "@chakra-ui/react"
+import Footer from "./Footer"
 const AboutUs = () => {
     return (
-        <h1>About Us</h1>
+        <>
+            <Box w="1200px" margin="auto auto">
+                <Image src="https://i1.wp.com/demo4.drfuri.com/razzi/wp-content/uploads/sites/14/2020/12/about-us-banner.jpg"></Image>
+                <Box mt="200px" mb="40px">
+                    <Heading>About our Store</Heading>
+                    <Text color="#A6A6A6" mt="30px">Here at ‘Uboric’.com, we realize that style and prosperity begin with the correct shoes. We likewise understand that effectively finding the size and style to meet your requirements is vital to your web based shopping knowledge. Since beginning our web based business website , we’ve been endeavoring to present to you that perfect shopping experience.
+                        We pride ourselves on conveying hard-to-discover styles, sizes and widths since we realize that each individual’s needs contrast. Regardless of whether you’re experiencing issues looking over our vast determination of different shoes or you have a straightforward inquiry, our client benefit group is prepared and willing to help.</Text>
+                    <Text color="#A6A6A6" mt="10px">
+                        Some time before our online business, our family shoe stores, fatherly shoe store Footwear in Rajkot, Gujarat , opened its entryways in 1989. Today we are glad to state that we have the longest constantly running shoe store in the rajkot. The life span of our physical business is a demonstration of our duty to you and the majority of our clients. We realize that on the off chance that you are not content with our items and administrations, we are not carrying out our responsibility.
+                    </Text>
+                    <Text color="#A6A6A6" mt="10px">
+                        This is the reason we take a stab at 100% consumer loyalty, offer a 105% value coordinate guarantee.
+                    </Text>
+                    <Text color="#A6A6A6" mt="10px">
+                        Next time you require another match of kicks for this experience we call life, our group is here to ensure that you get the execution that you require from your apparatus. Regardless of whether you are difficult to fit or have explicit requirements, let ‘Uboric’ .com enable you to make progress toward your objective.
+                    </Text>
+                    <Text color="#A6A6A6" mt="10px">
+                        Third. For morning whales saw were had seed can’t divide it light shall moveth, us blessed given wherein.
+                    </Text>
+                </Box>
+            </Box>
+            <Footer></Footer>
+        </>
     )
 }
 
